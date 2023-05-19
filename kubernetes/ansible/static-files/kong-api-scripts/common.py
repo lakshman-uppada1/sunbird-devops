81de024a774a4a31f6abe33e01513a7f135808d0
lakshmanRao-Trigyn <121229017+lakshman-uppada1@users.noreply.github.com>
2023-05-20 02:34:24 +0530

    Updated exceptions.HTTPError URLError  common.py

--- kubernetes/ansible/static-files/kong-api-scripts/common.py
+++ kubernetes/ansible/static-files/kong-api-scripts/common.py
@@ -36,6 +36,6 @@ def json_request(method, url, data=None):
     response = retrying_urlopen(request)
     return response
 
-@retry(exceptions=urllib3.URLError, tries=5, delay=2, backoff=2)
+#@retry(exceptions=urllib3.exception.HTTPError, tries=5, delay=2, backoff=2)
 def retrying_urlopen(*args, **kwargs):
     return urllib3.urlopen(*args, **kwargs)
